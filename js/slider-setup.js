1e4660ae3dd197706293510fe80a7d0e5ade88ff
Vitaliy Samus <vitaly992z@gmail.com>
2021-02-27 17:24:21 +0200

    added porfolio slider

--- js/slider-setup.js
+++ js/slider-setup.js
@@ -18,3 +18,19 @@ document.getElementById('comments_prev').onclick = function () {
 document.getElementById('comments_next').onclick = function () {
   commentsSlider.goTo('next');
 };
+
+const portfolioSlider = tns({
+  container: '.img_slider',
+  items: 1,
+  slideBy: 1,
+  autoplay: false,
+  autoplayButtonOutput: false,
+  autoplayHoverPause: true,
+  mouseDrag: true,
+  controls: true,
+  nav: true,
+  navContainer: '#portfolio-navcontainer',
+  controlsContainer: '#portfolio-controls-container',
+  prevButton: '#portfolio-prev',
+  nextButton: '#portfolio-next'
+});
